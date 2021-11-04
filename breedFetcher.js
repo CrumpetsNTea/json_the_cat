f8a1374bdd5c57dbc3ae0c84cfcec01397d61932
CrumpetsNTea <t.botell@gmail.com>
2021-11-04 16:43:05 -0400

    breedFetcher working properly if user inputs invalid name, now returns error message by having a conditional if data.length is 0, meaning the fetcher was unable to find anything on the passed in cat breed, then it returns the error message, whereas before it was just giving me a typeError because it would still try to pull the description from an empty object

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,17 +2,18 @@ const request = require('request');
 
 const fetchBreedDescription = (breedName, callback) => { //callback here is equal to (error, desc) in index.js
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-    const data = JSON.parse(body);
-    let cat = data[0];
-
     if (error) {//if there is an error then:
       callback(error, null); //if there is an error, then the callback function in index calls error and console.logs it and sets desc to null
       return;
     }
-    if (!error) {
-      callback(null, cat.description); //sets error to null and logs the cat description
+
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback('404 CAT NOT FOUND', null);
       return;
     }
+    let cat = data[0];
+    callback(null, cat.description); //sets error to null and logs the cat description
   });
 };
 
