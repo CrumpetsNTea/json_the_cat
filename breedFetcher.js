456b51d0eff60ef06a2f29dc712f83d492886a94
CrumpetsNTea <t.botell@gmail.com>
2021-11-04 15:02:16 -0400

    add dynamic breed name functionality

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,10 @@
 const request = require('request');
-let URL = 'https://api.thecatapi.com/v1/breeds/search?q=sib';
+const slicedInput = process.argv.slice(2);
+const catName = slicedInput[0].trim();
+let URL = `https://api.thecatapi.com/v1/breeds/search?q=${catName}`;
 
 request(URL, (error, response, body) => {
-  const obj = JSON.parse(body);
-  console.log(obj);
-
-
+  const data = JSON.parse(body);
+  let cat = data[0]; //access the object inside the array
+  console.log(cat.description); //once we have accessed the object we can log it's contents
 });
