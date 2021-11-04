89574bf03733d5306fb415c44a42a5401ca0a76b
CrumpetsNTea <t.botell@gmail.com>
2021-11-04 15:55:51 -0400

    working index.js file and working breedFetcher.js. Now writing tests with Mocha and Chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,19 @@
 const request = require('request');
-const slicedInput = process.argv.slice(2); //slice data crap at start of command line
-const catName = slicedInput[0].trim(); //extracts user input
-let URL = `https://api.thecatapi.com/v1/breeds/search?q=${catName}`; //passes in user input at end of query
 
-request(URL, (error, response, body) => {
+const fetchBreedDescription = (breedName, callback) => { //callback here is equal to (error, desc) in index.js
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    const data = JSON.parse(body);
+    let cat = data[0];
 
-  if (error) {//if there is an error then:
-    console.log(`Uh-Oh! There was an error! Abandon Ship and try again! 🤷`); //let the user know
-    return;
-  }
-  const data = JSON.parse(body); //turn the body from string to an array containing an object
-  let cat = data[0]; //access the object inside the array (which is at index 0 because it's the only thing inside the array)
-  if (cat) { //if cat returns true, meaning it finds information for the user's input then:
-    console.log(cat.description); //display that breeds description
-  }   else {
-    console.log(`Unfortunately, This Breed Does Not Exist 😯`); //if the breed does not exist then tells the user
-  }
-});
+    if (error) {//if there is an error then:
+      callback(error, null); //if there is an error, then the callback function in index calls error and console.logs it and sets desc to null
+      return;
+    }
+    if (!error) {
+      callback(null, cat.description); //sets error to null and logs the cat description
+      return;
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
